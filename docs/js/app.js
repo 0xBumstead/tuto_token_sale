bb7d27cd1495875b13dff0f7f262ff503bf036ad
fmcch <fmhcry@gmail.com>
2021-08-20 20:36:08 +0200

    Compile assets for Github Pages

--- docs/js/app.js
+++ docs/js/app.js
@@ -13,16 +13,18 @@ App = {
 	},
 
 	initWeb3: function() {
-		if (typeof web3 !== 'undefined') {
+//		if (typeof web3 !== 'undefined') {
 			// If a web3 instance is already provided by Meta Mask.
 //			web3 = new Web3(web3.currentProvider);
 			web3 = new Web3(window.ethereum);
-			App.web3Provider = web3.currentProvider;			
-		} else {
+//			App.web3Provider = web3.currentProvider;
+			App.web3Provider = window.ethereum;
+
+//		} else {
 			// Specify default instance if no web3 instance provided
-			App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
-			web3 = new Web3(App.web3Provider);
-		}
+//			App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+//			web3 = new Web3(App.web3Provider);
+//		}
 		return App.initContracts();
 	},
 
